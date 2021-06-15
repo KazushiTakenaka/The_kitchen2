098993f3b8e42e998c25c92defb8e92aae2786a5
“KazushiTakenaka” <take.kazushi@gmail.com>
2021-06-15 07:40:38 +0000

    “[stert]follows”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_054341) do
+ActiveRecord::Schema.define(version: 2021_06_15_072348) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "recipe_id"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2021_06_11_054341) do
     t.integer "user_id"
     t.integer "time"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
