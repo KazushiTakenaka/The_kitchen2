2c028bd4b1e162851f663d8090346ad606354222
EC2 Default User <ec2-user@ip-172-31-5-176.ap-northeast-1.compute.internal>
2021-06-15 06:52:48 +0000

    “[finish]likes”

--- Gemfile
+++ Gemfile
@@ -56,13 +56,13 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'pry-byebug'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
-gem 'pry-byebug'
 gem 'bootstrap', '~> 4.3.1'
 gem 'font-awesome-sass'
 gem 'dotenv-rails'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,10 +3,14 @@ class Recipe < ApplicationRecord
   has_many :materials, dependent: :destroy
   has_many :makes, dependent: :destroy
   has_many :photos, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  def liked_by?(user)
+    likes.where(user_id: user_id).exists?
+  end
   accepts_nested_attributes_for :makes, :materials, :photos, allow_destroy: true
-  
+
   validates :time, presence: true
   validates :name, presence: true
   # accepts_nested_attributes_for :materials, allow_destroy: true
-  # accepts_nested_attributes_for :photos, allow_destroy: true 
+  # accepts_nested_attributes_for :photos, allow_destroy: true
 end
